dc672ab8cfe11f33aea9421b51bc4a159f027c9a
Alex Jr <alexjunior2018@gmail.com>
2025-01-07 13:57:22 -0300

    commit initial

---
+++ curso-java/src/secao2/Variavel.java
@@ -0,0 +1,29 @@
+package secao2;
+
+public class Variavel {
+
+	public static void main(String[] args) {
+		
+		// 1 - o que são variaveis
+		// tipo -> nome -> atribuir um valor;
+		
+		String nome = "Matheus";
+		
+		// nome => "Matheus"
+		System.out.println(nome);
+		
+		// 2 = atribuição de variavel com outra
+		String teste = "Teste";
+		
+		String testando = teste;
+		
+		System.out.println(testando);
+		
+		long numeroGrande = 99999999999999999L;
+		
+		//int numeroGrandeDois = numeroGrande;
+		
+		System.out.println(numeroGrande);
+	}
+
+}
